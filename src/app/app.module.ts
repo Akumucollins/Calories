5b8e32becc4837d1c5692c5872a46dbf7a8fc454
Akumucollins <akumucollins001@gmail.com>
2020-11-06 01:24:53 +0300

    created a Category component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
