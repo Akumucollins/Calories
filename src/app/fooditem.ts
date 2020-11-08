d384b6468dc4d891989e603447fd1d70443d2ea3
Akumucollins <akumucollins001@gmail.com>
2020-11-08 04:59:30 +0300

    generated profile class and fooditem class

---
+++ src/app/fooditem.ts
@@ -0,0 +1,13 @@
+export class Fooditem {
+    constructor(
+        public id: any,
+        public url: any,
+        public name: any,
+        public category: any,
+        public carbohydrate: any,
+        public protein: any,
+        public status: any,
+        public calorie: any,
+        public quantity: any,
+    ) {}
+}
---
+++ src/app/profile.ts
@@ -0,0 +1,18 @@
+export class Profile {
+    constructor(
+        public id: any,
+        public url: any,
+        public user: any,
+        public image: any,
+        // tslint:disable-next-line: variable-name
+        public first_name: any,
+        // tslint:disable-next-line: variable-name
+        public last_name: any,
+        public status: any,
+        public email: any,
+        public weight: any,
+        public height: any,
+        // tslint:disable-next-line: variable-name
+        public birth_field: any,
+    ) {}
+}
