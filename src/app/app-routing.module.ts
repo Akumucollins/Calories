5aca28691967f6f3dab45462f4b0a343cae9db65
Akumucollins <akumucollins001@gmail.com>
2020-11-08 04:28:08 +0300

    created a routes to navigate to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ProfileComponent } from './components/profile/profile.component';
+import { CategoryComponent } from './components/category/category.component';
+import { FooditemComponent } from './components/fooditem/fooditem.component';
+import { UserFooditemComponent } from './components/user-fooditem/user-fooditem.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'fooditem', component: FooditemComponent },
+  { path: 'user-fooditem', component: UserFooditemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
