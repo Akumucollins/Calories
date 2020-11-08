c84207e916a8a4ba010a30cbd260969cb5325d3d
Akumucollins <akumucollins001@gmail.com>
2020-11-08 04:34:16 +0300

    registed the httpclient and form in the app moduke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,8 +9,19 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { CategoryComponent } from './components/category/category.component';
 import { FooditemComponent } from './components/fooditem/fooditem.component';
 import { UserFooditemComponent } from './components/user-fooditem/user-fooditem.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { SharedService } from './shared.service';
+
+import { EditCategoryComponent } from './components/category/edit-category/edit-category.component';
+import { ViewCategoryComponent } from './components/category/view-category/view-category.component';
+import { EditFooditemComponent } from './components/fooditem/edit-fooditem/edit-fooditem.component';
+import { ViewFooditemComponent } from './components/fooditem/view-fooditem/view-fooditem.component';
+import { EditProfileComponent } from './components/profile/edit-profile/edit-profile.component';
+import { ViewProfileComponent } from './components/profile/view-profile/view-profile.component';
+import { ViewUserfooditemComponent } from './components/user-fooditem/view-userfooditem/view-userfooditem.component';
+import { EditUserfooditemComponent } from './components/user-fooditem/edit-userfooditem/edit-userfooditem.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -17,13 +30,26 @@ import { SharedService } from './shared.service';
     CategoryComponent,
     FooditemComponent,
     UserFooditemComponent,
-    NavbarComponent
+    EditCategoryComponent,
+    ViewCategoryComponent,
+    EditFooditemComponent,
+    ViewFooditemComponent,
+    EditProfileComponent,
+    ViewProfileComponent,
+    ViewUserfooditemComponent,
+    EditUserfooditemComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [SharedService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
